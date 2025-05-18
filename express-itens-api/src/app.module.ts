c29d23fa1eadb2bb4c1269251b8399f2695897ee
Belicio B Cardoso <belloinfo@gmail.com>
2025-05-17 21:30:25 -0300

    Configurando o Prisma Client

--- express-itens-api/src/app.module.ts
+++ express-itens-api/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './database/prisma.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -9,8 +11,9 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
     }),
     UserModule,
+    DatabaseModule,
   ],
   controllers: [],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
 })
 export class AppModule {}
